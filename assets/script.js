a0ab2941b852b452551db8dd126d3f64e62931bb
winwinchit1355 <winwinchit1355@gmail.com>
2023-09-21 00:15:34 +0630

    fix bug

--- assets/script.js
+++ assets/script.js
@@ -13,4 +13,11 @@ $(document).ready(function () {
         $('.menu-link').removeClass('active');
         $(this).addClass('active');
     })
+    $('.typing-text').on('animationend webkitAnimationEnd', function() {
+        // After the typing animation, show the intro-text
+        $('.intro-text').css({
+            opacity: 1,
+            transform: 'translateY(0)',
+        });
+    });
 });
