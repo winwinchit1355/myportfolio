479c8260803e3fb941077dcf154795714e0fb31a
winwinchit1355 <winwinchit1355@gmail.com>
2023-09-25 17:00:06 +0630

    change theme toggle

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+    $('#home-link').addClass('active');
+    
     // loader
     setTimeout(function () {
         $('#loader').css({"display":"none"});
@@ -8,53 +10,30 @@ $(document).ready(function () {
         $('#sidebar').addClass('open');
         $('#sidebarCollapse').addClass('open');
         $('#content').addClass('open');
+
+        const mode = window.localStorage.getItem('mode');
+        if (mode === 'light') {
+            changeTheme.prop('checked', true);
+            $('body').removeClass("dark").addClass("light");
+            darkToLight();
+        }else{
+            changeTheme.prop('checked', false);
+            $('body').removeClass("light").addClass("dark");
+            lightToDark();
+        }
     }, 3200);
-
-
-    $('#home-link').addClass('active');
 
     $("#sidebar").mCustomScrollbar({
         theme: "minimal"
     });
 
-    $('#sidebarCollapse').on('click', function () {
-        $('#sidebar, #content' ).toggleClass('active');
-        $(this).toggleClass('active');
-    });
-    $('.menu-link').click(function(){
-        $('.menu-link').removeClass('active');
-        $(this).addClass('active');
-    })
-    // theme change 
-    const theme = $('#theme');
-    const changeTheme = $('#mode');
 
-    changeTheme.on('change', function() {
-        if ($(this).prop('checked')) {
-        console.log("Checked");
-        $('body').removeClass("dark").addClass("light");
-        window.localStorage.setItem('mode', 'light');
-        } else {
-        console.log("Not Checked");
-        $('body').removeClass("light").addClass("dark");
-        window.localStorage.setItem('mode', 'dark');
-        }
-    });
-
-    const mode = window.localStorage.getItem('mode');
-    if (mode === 'dark') {
-        changeTheme.prop('checked', false);
-        $('body').removeClass("light").addClass("dark");
-    } else if (mode === 'light') {
-        changeTheme.prop('checked', true);
-        $('body').removeClass("dark").addClass("light");
-    }
-    $('.moon').click(function() {
-        var currentPosition = parseInt($(this).css('left'));
-        var newPosition = currentPosition + 50; // Adjust the amount to move
+    // $('#moon').click(function() {
+    //     var currentPosition = parseInt($(this).css('left'));
+    //     var newPosition = currentPosition + 50; // Adjust the amount to move
     
-        $(this).animate({ left: newPosition + 'px' }, 1000); // Adjust the duration as needed
-      });
+    //     $(this).animate({ left: newPosition + 'px' }, 1000); // Adjust the duration as needed
+    //   });
 
     $('.typing-text').on('animationend webkitAnimationEnd', function() {
         // After the typing animation, show the intro-text
@@ -85,3 +64,37 @@ $(document).ready(function () {
     
     
 });
+$('#sidebarCollapse').on('click', function () {
+    $('#sidebar, #content' ).toggleClass('open');
+    $(this).toggleClass('open');
+});
+$('.menu-link').click(function(){
+    $('.menu-link').removeClass('active');
+    $(this).addClass('active');
+})
+// theme change 
+const changeTheme = $('#mode');
+
+changeTheme.on('change', function() {
+    if ($(this).prop('checked')) {
+    // console.log("Checked");
+    $('body').removeClass("dark").addClass("light");
+    darkToLight();
+    window.localStorage.setItem('mode', 'light');
+    } else {
+    // console.log("Not Checked");
+    $('body').removeClass("light").addClass("dark");
+    lightToDark();
+    window.localStorage.setItem('mode', 'dark');
+    }
+});
+function darkToLight(){
+    $('#moon').removeClass('fa-spin');
+    $('#sun').addClass('fa-spin');
+    console.log($('#sun').attr('class'));
+}
+function lightToDark(){
+    $('#sun').removeClass('fa-spin');
+    $('#moon').addClass('fa-spin');
+    console.log($('#moon').attr('class'));
+}
